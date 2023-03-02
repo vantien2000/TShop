e89a2d1515d2ba0b6ca58c7700aa292f416e76bc
vantien2000 <vantienn740@gmail.com>
2023-03-02 16:25:08 +0700

    Add first project

---
+++ TShop.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TShop.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
